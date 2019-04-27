4152289bc783718846ea6d0e1865c4bac6477d7d
magiqfoxx <katarzyna.zemsta@vp.pl>
2019-04-27 23:00:26 +0200

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-let root = document.documentElement;
-
-root.addEventListener("mousemove", e => {
-  root.style.setProperty("--mouse-x", e.clientX + "px");
-  root.style.setProperty("--mouse-y", e.clientY + "px");
-});
