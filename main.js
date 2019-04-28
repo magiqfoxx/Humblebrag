d856b07664f8079f8bcdb27808aa8e79150f5df3
magiqfoxx <katarzyna.zemsta@vp.pl>
2019-04-28 23:53:53 +0200

    101

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
+function changeColors() {
+  document.documentElement.style.setProperty("--primary-color", "#085f63");
+  document.documentElement.style.setProperty("--secondary-color", "#49beb7");
+  document.documentElement.style.setProperty("--light-gray", "#f1f1f1");
+  document.documentElement.style.setProperty("--dark-gray", "#f5e1da");
+}
+function changeColors2() {
+  document.documentElement.style.setProperty("--primary-color", "#4c8492");
+  document.documentElement.style.setProperty("--secondary-color", "#f09c67");
+  document.documentElement.style.setProperty("--light-gray", "#f7e0a380");
+  document.documentElement.style.setProperty("--dark-gray", "#fffde8");
+}
+function changeColors3() {
+  document.documentElement.style.setProperty("--primary-color", "#305f72");
+  document.documentElement.style.setProperty("--secondary-color", "#f18c8e");
+  document.documentElement.style.setProperty("--light-gray", "#f1d1b58c");
+  document.documentElement.style.setProperty("--dark-gray", "#f1d1b5");
+}
+//
+/*
 let root = document.documentElement;
-
 root.addEventListener("mousemove", e => {
   root.style.setProperty("--mouse-x", e.clientX + "px");
   root.style.setProperty("--mouse-y", e.clientY + "px");
 });
+*/
